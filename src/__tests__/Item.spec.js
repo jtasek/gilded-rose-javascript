cde7a0dec898799d7b40a0bf5d751a104a70195d
Jakub Tasek <jakub.tasek@gmail.com>
2018-10-22 14:44:09 +0200

    refactor tests

--- src/__tests__/Item.spec.js
+++ src/__tests__/Item.spec.js
@@ -1,43 +1,51 @@
 import { Item } from '../Item'
 
-it('Item should be defined', function() {
-  let item = new Item()
+describe('Item class', () => {
+  it('Item should be defined', function() {
+    let item = new Item()
 
-  expect(item).toBeDefined()
-})
+    expect(item).toBeDefined()
+  })
 
-it('quality should be undefined when not specified in the constructor', function() {
-  let item = new Item()
+  describe('quality property', () => {
+    it('quality should be undefined when not specified in the constructor', function() {
+      let item = new Item()
 
-  expect(item.quality).toBeUndefined()
-})
+      expect(item.quality).toBeUndefined()
+    })
 
-it('sellIn should be undefined when not specified in the constructor', function() {
-  let item = new Item()
+    it('quality should be defined when specified in the constructor', function() {
+      let item = new Item('foo', 0, 1)
 
-  expect(item.sellIn).toBeUndefined()
-})
+      expect(item.quality).toBe(1)
+    })
+  })
 
-it('name should be undefined when not specified in the constructor', function() {
-  let item = new Item()
+  describe('sellIn property', () => {
+    it('sellIn should be undefined when not specified in the constructor', function() {
+      let item = new Item()
 
-  expect(item.name).toBeUndefined()
-})
+      expect(item.sellIn).toBeUndefined()
+    })
 
-it('quality should be defined when specified in the constructor', function() {
-  let item = new Item('foo', 0, 1)
+    it('sellIn should be defined when specified in the constructor', function() {
+      let item = new Item('foo', 1, 0)
 
-  expect(item.quality).toBe(1)
-})
+      expect(item.sellIn).toBe(1)
+    })
+  })
 
-it('sellIn should be defined when specified in the constructor', function() {
-  let item = new Item('foo', 1, 0)
+  describe('name property', () => {
+    it('name should be undefined when not specified in the constructor', function() {
+      let item = new Item()
 
-  expect(item.sellIn).toBe(1)
-})
+      expect(item.name).toBeUndefined()
+    })
 
-it('name should be defined when specified in the constructor', function() {
-  let item = new Item('foo')
+    it('name should be defined when specified in the constructor', function() {
+      let item = new Item('foo')
 
-  expect(item.name).toBe('foo')
+      expect(item.name).toBe('foo')
+    })
+  })
 })
--- src/__tests__/Shop.spec.js
+++ src/__tests__/Shop.spec.js
@@ -1,131 +1,137 @@
 import { Shop } from '../Shop'
 import { Item } from '../Item'
 
-describe('Gilded Rose shop', function() {
-  it('shop should be defined', function() {
-    const shop = new Shop()
+describe('Shop class', function() {
+  describe('Constructor tests', () => {
+    it('shop should be defined', function() {
+      const shop = new Shop()
 
-    expect(shop).toBeDefined()
+      expect(shop).toBeDefined()
+    })
+
+    it('shop items should be defined when not specified in constructor', function() {
+      const shop = new Shop()
+
+      expect(shop.items).toBeDefined()
+    })
+
+    it('shop items should be stored as is', function() {
+      let expected = new Item('foo', 1, 1)
+      const shop = new Shop([expected])
+      const actual = shop.items[0]
+
+      expect(actual).toEqual(expected)
+    })
+
+    it('shop items count should remain unchanged', function() {
+      const shop = new Shop([new Item('foo', 1, 1)])
+
+      expect(shop.items.length).toBe(1)
+    })
   })
 
-  it('shop items should be defined when not specified in constructor', function() {
-    const shop = new Shop()
+  describe('updateQuality method tests', () => {
+    it('updateQuality method should not modify item name', function() {
+      const shop = new Shop([new Item('foo', 1, 1)])
+      const items = shop.updateQuality()
 
-    expect(shop.items).toBeDefined()
-  })
+      expect(items[0].name).toEqual('foo')
+    })
 
-  it('shop items should be stored as is', function() {
-    let expected = new Item('foo', 1, 1)
-    const shop = new Shop([expected])
-    const actual = shop.items[0]
+    it('updateQuality method should decrease the quality of an item each day', function() {
+      const shop = new Shop([new Item('foo', 1, 1)])
 
-    expect(actual).toEqual(expected)
-  })
+      const items = shop.updateQuality()
 
-  it('shop items count should remain unchanged', function() {
-    const shop = new Shop([new Item('foo', 1, 1)])
+      expect(items[0].quality).toBe(0)
+    })
 
-    expect(shop.items.length).toBe(1)
-  })
+    it('updateQuality method should decrease the sellIn of an item each day', function() {
+      const shop = new Shop([new Item('foo', 1, 1)])
 
-  it('updateQuality method should not modify item name', function() {
-    const shop = new Shop([new Item('foo', 1, 1)])
-    const items = shop.updateQuality()
+      const items = shop.updateQuality()
 
-    expect(items[0].name).toEqual('foo')
-  })
+      expect(items[0].sellIn).toBe(0)
+    })
 
-  it('updateQuality method should decrease the quality of an item each day', function() {
-    const shop = new Shop([new Item('foo', 1, 1)])
+    it('quality of an item is never negative', function() {
+      const shop = new Shop([new Item('foo', 1, 0)])
 
-    const items = shop.updateQuality()
+      const items = shop.updateQuality()
 
-    expect(items[0].quality).toBe(0)
-  })
+      expect(items[0].quality).toBe(0)
+    })
 
-  it('updateQuality method should decrease the sellIn of an item each day', function() {
-    const shop = new Shop([new Item('foo', 1, 1)])
+    it('quality of an item is never more than 50', function() {
+      const shop = new Shop([new Item('foo', 1, 50)])
 
-    const items = shop.updateQuality()
+      const items = shop.updateQuality()
 
-    expect(items[0].sellIn).toBe(0)
-  })
+      expect(items[0].quality).toBeLessThan(50)
+    })
 
-  it('Quality of an item is never negative', function() {
-    const shop = new Shop([new Item('foo', 1, 0)])
+    it('"Aged Brie" quality is never more than 50', function() {
+      const shop = new Shop([new Item('Aged Brie', 1, 50)])
 
-    const items = shop.updateQuality()
+      const items = shop.updateQuality()
 
-    expect(items[0].quality).toBe(0)
-  })
+      expect(items[0].quality).toBe(50)
+    })
 
-  it('quality of an item is never more than 50', function() {
-    const shop = new Shop([new Item('foo', 1, 50)])
+    it('"Sulfuras" never decreases in Quality', function() {
+      const shop = new Shop([new Item('Sulfuras, Hand of Ragnaros', 1, 50)])
 
-    const items = shop.updateQuality()
+      const items = shop.updateQuality()
 
-    expect(items[0].quality).toBeLessThan(50)
-  })
+      expect(items[0].quality).toBe(50)
+    })
 
-  it('"Aged Brie" quality is never more than 50', function() {
-    const shop = new Shop([new Item('Aged Brie', 1, 50)])
+    it('"Sulfuras" never decreases in Sellin', function() {
+      const shop = new Shop([new Item('Sulfuras, Hand of Ragnaros', 1, 50)])
 
-    const items = shop.updateQuality()
+      const items = shop.updateQuality()
 
-    expect(items[0].quality).toBe(50)
-  })
+      expect(items[0].sellIn).toBe(1)
+    })
 
-  it('"Sulfuras" never decreases in Quality', function() {
-    const shop = new Shop([new Item('Sulfuras, Hand of Ragnaros', 1, 50)])
+    describe('Backstage passes', () => {
+      it('"Backstage passes" Quality increases by 2 when there are 10 days or less', function() {
+        const shop = new Shop([
+          new Item('Backstage passes to a TAFKAL80ETC concert', 10, 0)
+        ])
 
-    const items = shop.updateQuality()
+        const items = shop.updateQuality()
 
-    expect(items[0].quality).toBe(50)
-  })
+        expect(items[0].quality).toBe(2)
+      })
 
-  it('"Sulfuras" never decreases in Sellin', function() {
-    const shop = new Shop([new Item('Sulfuras, Hand of Ragnaros', 1, 50)])
+      it('"Backstage passes" Quality increases by 3 when there are 5 days or less', function() {
+        const shop = new Shop([
+          new Item('Backstage passes to a TAFKAL80ETC concert', 5, 0)
+        ])
 
-    const items = shop.updateQuality()
+        const items = shop.updateQuality()
 
-    expect(items[0].sellIn).toBe(1)
-  })
+        expect(items[0].quality).toBe(3)
+      })
 
-  it('"Backstage passes" Quality increases by 2 when there are 10 days or less', function() {
-    const shop = new Shop([
-      new Item('Backstage passes to a TAFKAL80ETC concert', 10, 0)
-    ])
+      it('"Backstage passes" Quality drops to 0 after the concert', function() {
+        const shop = new Shop([
+          new Item('Backstage passes to a TAFKAL80ETC concert', 0, 5)
+        ])
 
-    const items = shop.updateQuality()
+        const items = shop.updateQuality()
 
-    expect(items[0].quality).toBe(2)
-  })
+        expect(items[0].quality).toBe(0)
+      })
+    })
 
-  it('"Backstage passes" Quality increases by 3 when there are 5 days or less', function() {
-    const shop = new Shop([
-      new Item('Backstage passes to a TAFKAL80ETC concert', 5, 0)
-    ])
+    it('Once the sell by date has passed, Quality degrades twice as fast', function() {
+      const shop = new Shop([new Item('foo', -1, 5)])
 
-    const items = shop.updateQuality()
+      const items = shop.updateQuality()
 
-    expect(items[0].quality).toBe(3)
-  })
-
-  it('"Backstage passes" Quality drops to 0 after the concert', function() {
-    const shop = new Shop([
-      new Item('Backstage passes to a TAFKAL80ETC concert', 0, 5)
-    ])
-
-    const items = shop.updateQuality()
-
-    expect(items[0].quality).toBe(0)
-  })
-
-  it('Once the sell by date has passed, Quality degrades twice as fast', function() {
-    const shop = new Shop([new Item('foo', -1, 5)])
-
-    const items = shop.updateQuality()
-
-    expect(items[0].quality).toBe(3)
+      expect(items[0].quality).toBe(3)
+    })
   })
 })
