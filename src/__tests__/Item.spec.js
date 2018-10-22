e551debd1b7f2a7f5d6ac1137020b23f349a46c7
Jakub Tasek <jakub.tasek@gmail.com>
2018-10-22 15:31:28 +0200

    test refactoring

--- src/__tests__/Item.spec.js
+++ src/__tests__/Item.spec.js
@@ -1,51 +1,57 @@
 import { Item } from '../Item'
 
 describe('Item class', () => {
-  it('Item should be defined', function() {
+  it('Item is defined', function() {
     let item = new Item()
 
     expect(item).toBeDefined()
   })
 
   describe('quality property', () => {
-    it('quality should be undefined when not specified in the constructor', function() {
+    let quality = 1
+
+    it('quality is undefined when not specified in the constructor', function() {
       let item = new Item()
 
       expect(item.quality).toBeUndefined()
     })
 
-    it('quality should be defined when specified in the constructor', function() {
-      let item = new Item('foo', 0, 1)
+    it('quality is defined when specified in the constructor', function() {
+      let item = new Item('foo', 0, quality)
 
-      expect(item.quality).toBe(1)
+      expect(item.quality).toBe(quality)
     })
   })
 
   describe('sellIn property', () => {
-    it('sellIn should be undefined when not specified in the constructor', function() {
+    let sellin = 1
+
+    it('sellIn is undefined when not specified in the constructor', function() {
       let item = new Item()
 
       expect(item.sellIn).toBeUndefined()
     })
 
-    it('sellIn should be defined when specified in the constructor', function() {
-      let item = new Item('foo', 1, 0)
+    it('sellIn is defined when specified in the constructor', function() {
+      let item = new Item('foo', sellin, 0)
 
-      expect(item.sellIn).toBe(1)
+      expect(item.sellIn).toBe(sellin)
     })
   })
 
   describe('name property', () => {
-    it('name should be undefined when not specified in the constructor', function() {
+    let name = 'foo'
+
+    it('name is undefined when not specified in the constructor', function() {
       let item = new Item()
 
       expect(item.name).toBeUndefined()
     })
 
-    it('name should be defined when specified in the constructor', function() {
-      let item = new Item('foo')
+    it('name is defined when specified in the constructor', function() {
+      let item = new Item(name, 0, 0)
 
-      expect(item.name).toBe('foo')
+      expect(item.name).toBe(name)
     })
   })
 })
